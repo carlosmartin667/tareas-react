d9a07872537f7f2b684fb24822933ee369ce0df2
carlosmartin667 <46163584+carlosmartin667@users.noreply.github.com>
2022-01-06 18:14:57 -0300

    agregando tareas y listando

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,77 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import apiFirebase from "./db/apiFirebase";
 
 const App = () => {
+  const [data, setdata] = useState([]);
+  const [tarea, settarea] = useState("");
+
+  const getData = async () => {
+    setdata(await apiFirebase.obtenerDatos());
+  };
+
+  const addTarea = async (e) => {
+    e.preventDefault();
+    if (!tarea.trim() ) {
+      return;
+    }
+    try {
+      await apiFirebase.agregarDatos(tarea);
+      settarea("");
+      setdata(await apiFirebase.obtenerDatos());
+    } catch (error) {
+      console.log(error);
+    }
+      
+    
+    console.log("tarea", tarea);
+  }
   useEffect(() => {
-    var x = apiFirebase.optenerDatos();
-    console.log(x);
+    getData();
   }, []);
 
   return (
     <div id="App" className="container">
-      <h1>hola</h1>
+      <div className="row">
+        <h1>hola</h1>
+        <div className="col-5">
+          <table className="table table-success table-striped table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, index) => (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>{item.name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="col-6">
+          <div className="card text-white bg-success ">
+            <div className="card-header">Tarea Agregar</div>
+            <div className="card-body">
+              <form onSubmit={addTarea}>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="tarea"
+                  onChange={(e) => settarea(e.target.value)}
+                  value={tarea}
+                />
+              </form>
+            </div>
+            <div className="card-footer bg-transparent border-success">
+              <button className="btn btn-dark btn-block mh-2">Agregar</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/db/apiFirebase.js
+++ src/db/apiFirebase.js
@@ -1,14 +1,28 @@
 import { firebase } from "./firebase";
 
-const optenerDatos = async () => {
+const obtenerDatos = async () => {
   try {
     const db = firebase.firestore();
     const data = await db.collection("tareas").get();
-    return data
+    const arrayData = data.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    return arrayData;
   } catch (error) {
     console.log(error);
   }
 };
-const apiFirebase = { optenerDatos };
+
+const agregarDatos = async (tarea) => {
+  try {
+    const db = firebase.firestore();
+    const nuevaTarea = {
+      name: tarea,
+      fecha: Date.now(),
+    };
+    const data = await db.collection("tareas").add(nuevaTarea);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const apiFirebase = { obtenerDatos, agregarDatos };
 
 export default apiFirebase;
